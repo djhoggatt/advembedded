d4d08dce3b516925f316ff0fd8261d929b0181f6
djhoggatt <djhoggatt@gmail.com>
2014-10-06 08:30:56 -0600

    Create client_old.c

---
+++ uart/client_old.c
@@ -0,0 +1,123 @@
+//Based on http://bradsmc.blogspot.com/2013/11/c-code-to-read-gps-data-via-serial-on.html
+//and https://groups.google.com/forum/#!topic/beagleboard/I26rrzcpr-g
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BAUDRATE B9600   // Change as needed, keep B
+#define MODEMDEVICE "/dev/ttyO1" //Beaglebone Black serial port
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+int rot13(char c)
+{
+  if('a' <= c && c <= 'z')
+  {
+    return (((c - 'a') + 13) % 26) + 'a';
+  } 
+  else if ('A' <= c && c <= 'Z') 
+  {
+    return (((c - 'A') + 13) % 26) + 'A';
+  } 
+  else 
+  {
+    return c;
+  }
+}
+
+main()
+{
+    //Initialize Values
+    int fd, c, res, i;
+    struct termios oldtio, newtio;
+    char *in;
+    char out;
+
+    // Load the pin configuration
+    int ret = system("echo uart1 > /sys/devices/bone_capemgr.9/slots");
+
+    //Open the device
+    fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY );
+    if (fd < 0) { perror(MODEMDEVICE); exit(-1); }
+
+    //clear struct for new settings
+    bzero(&newtio, sizeof(newtio)); 
+
+    //BAUDRATE: Set bps rate. You could also use cfsetispeed and cfsetospeed.
+    //CRTSCTS : output hardware flow control (only used if the cable has
+    //          all necessary lines. See sect. 7 of Serial-HOWTO)
+    //CS8     : 8n1 (8bit,no parity,1 stopbit)
+    //CLOCAL  : local connection, no modem contol
+    //CREAD   : enable receiving characters
+    newtio.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;
+
+    //IGNPAR  : ignore bytes with parity errors
+    //otherwise make device raw (no other input processing)
+    newtio.c_iflag = IGNPAR;
+
+    //  Raw output
+    newtio.c_oflag = 0;
+
+    //ICANON  : enable canonical input
+    //Disable all echo functionality, and don't send signals to calling program
+    newtio.c_lflag = ICANON;
+
+    //Now clean the modem line and activate the settings for the port
+    tcflush(fd, TCIFLUSH);
+    tcsetattr(fd,TCSANOW,&newtio);
+
+    //Enter main while loop
+    while (TRUE) {
+        //Read input from user
+	printf ("User Input: ");
+        gets(in);
+        strcat(in,"\n");
+	printf("Got Here, with value: %s", in);
+
+        //Encrypt
+        for(i = 0; in[i]!= '\0'; i++)
+        { 
+	    in[i] = rot13(in[i]);
+        }
+         
+	//Write input to UART
+	write(fd, in, strlen(in));
+	for(i = 0; i < 3000000; i++);
+	//Read the input from the UART
+	//Note, that the read blocks until a newline character is read.
+        //fgets(out,5,);
+	printf("Recieved From Server: ");
+	while(out != '\n')
+	{
+                read(fd,&out, 1);
+
+		//Print out the output
+		printf("%c", out);
+        }
+	printf("\n");
+	out = '\0';
+	/*
+	int i;
+	char * s;
+	scanf("%s", s);
+	for(i = 0; i < sizeof(*s); i++)
+	{
+		//We might need to use this approach
+	}
+        write(fd, *s, sizeof(*s));
+	for(i = 0; i < 3000000; i++);*/
+    }
+
+    //restore old port settings
+    tcsetattr(fd,TCSANOW, &oldtio);
+
+    //Close the device
+    close (fd);
+}
