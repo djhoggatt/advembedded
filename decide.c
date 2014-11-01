233265e172960a12543c8ea612b680abd98088e0
coltonottley <coltonottley@gmail.com>
2014-11-01 12:15:59 -0600

    lic14 added
    I added LIC14 and removed the print statements for the LIC1 coverage
    test.

--- decide.c
+++ decide.c
@@ -5,7 +5,6 @@
 */
 
 #include "decide.h"
-#include <stdio.h>
 
 // -- Forward Declarations -- //
 boolean LIC0();
@@ -14,6 +13,16 @@ boolean LIC2();
 boolean LIC3();
 boolean LIC4();
 boolean LIC5();
+boolean LIC6();
+boolean LIC7();
+boolean LIC8();
+boolean LIC9();
+boolean LIC10();
+boolean LIC11();
+boolean LIC12();
+boolean LIC13();
+boolean LIC14();
+
 
 
 boolean all_elements_in_row_are_true(BMATRIX, int);
@@ -126,9 +135,6 @@ boolean LIC1()
 		ch=  DOUBLECOMPARE(a,b);
 		if(ch == EQ)
 		{
-			//makes a line
-			//got here
-			printf("makes a line\n");
 			// Decidesthat it is a Line So compute the greatest separation between the points to find length of the line
 			a = length_point(X[i],Y[i],X[i+1],Y[i+1]);
 			b = length_point(X[i],Y[i],X[i+2],Y[i+2]);
@@ -137,71 +143,49 @@ boolean LIC1()
 			ch1 = DOUBLECOMPARE(a,b);
 			if (ch1 == GT)
 			{ 
-				//Compares the greater of the 2 with the third
-				//got here
-				printf("a is greater than b\n");
 				ch2 = DOUBLECOMPARE(a,r);
 				if(ch2 == GT)
 				{
-					//got here
-					printf("a is greater than r\n");
 					//For a Circle to be Able to encompass the 3 points,
 					// Radius of circle must be greater than or equal to half the length of the Line
 					ch3 = DOUBLECOMPARE((a/2),PARAMETERS.RADIUS1);
 					if (ch3 == GT)
 					{
-						//got here
-						printf("a/2 is greater than RADIUS1\n");
 						return 1;
 					}
 				}
 				else
 				{
-					//got here
-					printf("r is greater than or equal to a\n");
 					//For a Circle to be Able to encompass the 3 points,
 					// Radius of circle must be greater than or equal to half the length of the Line
 					ch3 = DOUBLECOMPARE((r/2),PARAMETERS.RADIUS1);
 					if (ch3 == GT)
 					{
-						//got here
-						printf("r/2 is greater than RADIUS1\n");
 						return 1;
 					}
 				}
 			}
 			else
 			{
-				//got here b is greater than a
-				printf("b is greater than or equal to a\n");
 				//Compares the greater of the 2 with the third
 				ch2 = DOUBLECOMPARE(b,r);
 				if(ch2 == GT)
 				{
-					//got here
-					printf("b is greater than r\n");
 					//For a Circle to be Able to encompass the 3 points,
 					// Radius of circle must be greater than or equal to half the length of the Line
 					ch3 = DOUBLECOMPARE((b/2),PARAMETERS.RADIUS1);
 					if (ch3 == GT)
 					{
-						//got here
-						printf("b/2 is greater than RADIUS1\n");
 						return 1;
 					}
 				}
 				else
 				{
-					//got here
-					//r is greater than b
 					//For a Circle to be Able to encompass the 3 points,
 					// Radius of circle must be greater than or equal to half the length of the Line
-					printf("r is greater than or equal to b\n");
 					ch3 = DOUBLECOMPARE((r/2),PARAMETERS.RADIUS1);
 					if (ch3 == GT)
 					{
-						//got here
-						printf("r/2 is greater than RADIUS1\n");
 						return 1;
 					}
 				}
@@ -209,54 +193,42 @@ boolean LIC1()
 		}
 		else
 		{ 
-			printf("forms a triangle\n");
 			//The Points form a Triangle. All Triangles are Circumscribable
 			// For the 3 points to be contained,
 			// the Radius of the Circle should be greater than or equal to the Circumradius if triangle is acute or right.
 			// For obtuse, Radius of circle must be greater than halff the length of the longest side
 			double ang1,ang2,ang3;
 			ang1 = angle_points(X[i],Y[i],X[i+1],Y[i+1],X[i+2],Y[i+2]);
-			printf("angle1 = %f\n", ang1);
 			ang2 = angle_points(X[i],Y[i],X[i+2],Y[i+2],X[i+1],Y[i+1]);
-			printf("angle2 = %f\n", ang2);
 			ang3 = angle_points(X[i+1],Y[i+1],X[i],Y[i],X[i+2],Y[i+2]);
-			printf("angle3 = %f\n", ang3);
 			if((DOUBLECOMPARE(ang1,PI/2)<GT)&&(DOUBLECOMPARE(ang2,PI/2)<GT)&&(DOUBLECOMPARE(ang3,PI/2)<GT))
 			{
-				//got here
-				printf("all angles are less than or equal to 90 degrees\n");
 				r  = circumcenter(X[i],Y[i],X[i+1],Y[i+1],X[i+2],Y[i+2]);
 			}
 			else
 			{ 
 				if(DOUBLECOMPARE(ang1,PI/2)==GT)
 				{
-					printf("ang1 is obtuse\n"); 
 					r = (length_point(X[i],Y[i],X[i+2],Y[i+2]))/2;
 				}
 				else if(DOUBLECOMPARE(ang2,PI/2)==GT)
 				{
-					//got here
-					printf("ang2 is obtuse\n"); 
 					r = (length_point(X[i],Y[i],X[i+1],Y[i+1]))/2;
 				}
 				else
 				{
-					printf("ang3 is obtuse\n"); 
 					r = (length_point(X[i+1],Y[i+1],X[i+2],Y[i+2]))/2;
 				}
 
 			}
 			ch1 = DOUBLECOMPARE(PARAMETERS.RADIUS1,r);
 			if(ch1 == LT)
-			{
-				printf("RADIUS1 is less than r\n"); 
+			{ 
 				return 1;
 			}
 
 		}
 	}
-	printf("all sets of three consecutive data points can be contained within or on a circle of radius RADIUS1\n");
 	return 0;
 }
 
@@ -347,13 +319,47 @@ boolean LIC5()
 	return 0;
 }
 
+/* There exists at least one set of three data points, separated by exactly E_PTS and F_PTS consecutive intervening
+ * points, respectively, that are the vertices of a triangle with area greater than AREA1. In addition, there exist three
+ * data points (which can be the same or different from the three data points just mentioned) separated by exactly
+ * E PTS and F PTS consecutive intervening points, respectively, that are the vertices of a triangle with area less
+ * than AREA2. Both parts must be true for the LIC to be true. The condition is not met when NUMPOINTS <= 5.
+ * 0 <= AREA2
+ */
+boolean LIC14()
+{
+	boolean cond_1;
+	boolean cond_2;
+	double area;
+	if(NUMPOINTS < 6 || PARAMETERS.AREA2 <= 0)
+	{
+		return 0;
+	}
+	
+	int i;
+	for(i = 0; i < (NUMPOINTS - (PARAMETERS.E_PTS + PARAMETERS.F_PTS); i++)
+	{
+		area = Calculate_Area_Triangle(X[i], Y[i], X[i+PARAMETERS.E_PTS], Y[i+PARAMETERS.E_PTS], X[i+PARAMETERS.E_PTS+PARAMETERS.F_PTS], Y[i+PARAMETERS.E_PTS+PARAMETERS.F_PTS]);
+		if(DOUBLECOMPARE(area, PARAMETERS.AREA1) == GT)
+		{
+			cond_1 = 1;
+		}
+		if(DOUBLECOMPARE(area, PARAMETERS.AREA2) == LT)
+		{
+			cond_2 = 1;
+		}
+	}
+	
+	return (cond_1 & cond_2);
+}
+
 
 // -- Helper Functions -- //
 
 /*
 * This function calculates the area of a triangle
 * given three points in the same plane.
-* This function was implemented using basic mathematical formulae (by ourselves).
+* This function was implemented using basic mathematical formula (by ourselves).
 */
 double Calculate_Area_Triangle(double ax, double ay, double bx, double by, double cx, double cy)
 {
